44d5cf7a11ab2b58048d3aab2b802541080e06e5
sk.kwon <sk.kwon@battleent.com>
2020-01-31 15:02:28 +0900

    Initial project

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    java
+    `maven-publish`
+    id("org.jmailen.kotlinter") version "2.2.0"
+    kotlin("jvm") version "1.3.70-eap-42"
+}
+
+group = "network.piction"
+version = "1.0.0"
+
+repositories {
+    maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap") }
+    mavenCentral()
+}
+
+dependencies {
+    implementation("commons-codec:commons-codec:1.5")
+    implementation(kotlin("stdlib-jdk8"))
+    testCompile("junit", "junit", "4.12")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "github"
+            url = uri("https://maven.pkg.github.com/piction-protocol/piction-utils")
+            credentials {
+                username = findProperty("RESOURCES_USERNAME") as String
+                password = findProperty("RESOURCES_PASSWORD") as String
+            }
+        }
+    }
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
